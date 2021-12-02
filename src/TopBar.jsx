5544ebf4372291cff2ff1ed856a001f02e1b33e6
Harshal Lele <harshallele97@gmail.com>
2021-12-02 12:20:42 +0530

    style changes

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -2,10 +2,13 @@ import {useState, useEffect} from "react";
 
 function TopBar(props) {
   const [searchText,setSearch] = useState("");
+  const [order,setOrder] = useState(1);
 
   const callSort = (event,val) => {
+    if(event == "order") setOrder(val);
     props.sortFilter({event,val});
   }
+
     
   return (
     <div className="w-100 flex">
@@ -20,16 +23,16 @@ function TopBar(props) {
           onChange={(e) => setSearch(e.target.value)}
         />
         <button className="input btn-search" onClick={() => callSort("search",searchText)}>🔍</button>
+        <button className="input btn-search" onClick={() => {callSort("search",'');setSearch('');}}>╳</button>
       </div>
       <div className="sort_col flex">
         <select name="sort" id="sort" defaultValue="Value"  className="input input_sel" onChange={(e) => callSort("sort",e.target.value)}>
-          <option value="">Sort By</option>
           <option value="Value">Value</option>
           <option value="PFName">Name</option>
         </select>
         <div className="btn-group">
-          <button className="input btn" onClick={() => callSort("order",1)}>▲</button>
-          <button className="input btn" onClick={() => callSort("order",-1)}>▼</button>
+          <button className="input btn" onClick={() => callSort("order",1)} style={{backgroundColor: order == 1 ? 'grey' : 'initial'}}>▲</button>
+          <button className="input btn" onClick={() => callSort("order",-1)} style={{backgroundColor: order == -1 ? 'grey' : 'initial'}}>▼</button>
         </div>
       </div>
     </div>
